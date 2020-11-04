d12f136921892f70888b547b0287826477d6d549
Austin Scola <austinscola@gmail.com>
2020-11-03 21:50:53 -0500

    Add node children iteration

--- arborista/node.py
+++ arborista/node.py
@@ -1,5 +1,10 @@
 """A node in a tree."""
+from typing import Iterator, List
 
 
 class Node():  # pylint: disable=too-few-public-methods
     """A node in a tree."""
+    def iterate_children(self) -> Iterator['Node']:  # pylint: disable=no-self-use
+        """Yield children of this node."""
+        nodes: List[Node] = []
+        return iter(nodes)
--- tests/test_node.py
+++ tests/test_node.py
@@ -1,1 +1,11 @@
 """Test arborista.node."""
+from typing import Iterator
+
+from arborista.node import Node
+
+
+def test_node_iterate_children() -> None:
+    """Test arborista.node.iterate_children."""
+    node: Node = Node()
+    children_iterator: Iterator[Node] = node.iterate_children()
+    assert list(children_iterator) == []
