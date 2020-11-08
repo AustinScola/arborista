2d4da9937f95c16778c457c744ab928ebc29163d
Austin Scola <austinscola@gmail.com>
2020-11-07 19:17:21 -0500

    Make Python module statements optional
    Make the passing of statements when initializing a Python module
    optional. The default is to have an empty statement list.

--- arborista/nodes/python/module.py
+++ arborista/nodes/python/module.py
@@ -1,12 +1,17 @@
 """A Python module."""
-from typing import List
+from typing import Optional
 
 from arborista.node import Node
-from arborista.nodes.python.statement import Statement, Statements
+from arborista.nodes.python.statement import StatementList, Statements
 
 
 class Module(Node):  # pylint: disable=too-few-public-methods
     """A Python module."""
-    def __init__(self, name: str, statements: Statements):
+    def __init__(self, name: str, statements: Optional[Statements] = None):
         self.name: str = name
-        self.statements: List[Statement] = list(statements)
+
+        self.statements: StatementList
+        if statements is None:
+            self.statements = []
+        else:
+            self.statements = list(statements)
