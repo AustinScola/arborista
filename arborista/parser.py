333e34badfc08b34304d30baecaa0b3be8a5a99f
Austin Scola <austinscola@gmail.com>
2020-11-09 16:55:48 -0500

    Add parser class

---
+++ arborista/parser.py
@@ -0,0 +1,6 @@
+"""Converts unstructured data into structured data."""
+from abc import ABC
+
+
+class Parser(ABC):  # pylint: disable=too-few-public-methods
+    """Converts unstructured data into structured data."""
