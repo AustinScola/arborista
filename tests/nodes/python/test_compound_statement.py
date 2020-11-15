8ef4fa92d6c461b12b3bd3cbb483382ed8308350
Austin Scola <austinscola@gmail.com>
2020-11-15 07:42:49 -0500

    Add Python compound statement inheritance test

--- tests/nodes/python/test_compound_statement.py
+++ tests/nodes/python/test_compound_statement.py
@@ -1,3 +1,8 @@
 """Test arborista.nodes.python.compound_statement."""
-# pylint: disable=unused-import
 from arborista.nodes.python.compound_statement import CompoundStatement
+from arborista.nodes.python.statement import Statement
+
+
+def test_inheritance() -> None:
+    """Test arborista.nodes.python.compound_statement.CompoundStatement inheritance."""
+    assert issubclass(CompoundStatement, Statement)
