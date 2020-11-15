664519eb8384ed3ccac3ef3ad9fe8816247c4102
Austin Scola <austinscola@gmail.com>
2020-11-14 22:11:59 -0500

    Fix inheritance of Python name node

--- arborista/nodes/python/name.py
+++ arborista/nodes/python/name.py
@@ -1,8 +1,8 @@
 """A Python name."""
-from arborista.nodes.python.statement import Statement
+from arborista.nodes.python.atom import Atom
 
 
-class Name(Statement):  # pylint: disable=too-few-public-methods
+class Name(Atom):  # pylint: disable=too-few-public-methods
     """A Python name."""
     def __init__(self, value: str):
         self.value: str = value
