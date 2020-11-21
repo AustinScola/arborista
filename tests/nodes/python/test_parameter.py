255b0a58b444d898a8364598a06a6733ae787e98
Austin Scola <austinscola@gmail.com>
2020-11-21 15:26:44 -0500

    Add Python parameter name
    Add a name attribute to Python parameters

--- tests/nodes/python/test_parameter.py
+++ tests/nodes/python/test_parameter.py
@@ -1,4 +1,7 @@
 """Test arborista.nodes.python.parameter."""
+import pytest
+
+from arborista.nodes.python.name import Name
 from arborista.nodes.python.parameter import Parameter
 from arborista.nodes.python.python_node import PythonNode
 
@@ -6,3 +9,15 @@ from arborista.nodes.python.python_node import PythonNode
 def test_inheritance() -> None:
     """Test arborista.nodes.python.parameter.Parameter inheritance."""
     assert issubclass(Parameter, PythonNode)
+
+
+# yapf: disable # pylint: disable=line-too-long
+@pytest.mark.parametrize('name', [
+    (Name('foo')),
+])
+# yapf: enable # pylint: enable=line-too-long
+def test_init(name: Name) -> None:
+    """Test arborista.nodes.python.parameter.Parameter.__init__."""
+    parameter: Parameter = Parameter(name)
+
+    assert parameter.name == name
