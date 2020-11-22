c05841237b08cbe563901b189ca62b8035831c56
Austin Scola <austinscola@gmail.com>
2020-11-22 14:36:24 -0500

    Add Python return statement equality operator

--- arborista/nodes/python/return_statement.py
+++ arborista/nodes/python/return_statement.py
@@ -1,6 +1,12 @@
 """A Python return statement."""
+from typing import Any
+
 from arborista.nodes.python.flow_statement import FlowStatement
 
 
 class ReturnStatement(FlowStatement):  # pylint: disable=too-few-public-methods
     """A Python return statement."""
+    def __eq__(self, other: Any) -> bool:
+        if not isinstance(other, ReturnStatement):
+            return False
+        return True
