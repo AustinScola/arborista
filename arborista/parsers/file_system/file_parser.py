fd247e92f281c0051449ca42ec4a95378da4d07f
Austin Scola <austinscola@gmail.com>
2020-11-28 16:02:57 -0500

    Change file parsing to return a file

--- arborista/parsers/file_system/file_parser.py
+++ arborista/parsers/file_system/file_parser.py
@@ -1,14 +1,16 @@
 """Parses a file."""
 from pathlib import Path
 
+from arborista.nodes.file_system.file import File
 from arborista.parser import Parser
 
 
 class FileParser(Parser):  # pylint: disable=too-few-public-methods
     """Parses a file."""
     @staticmethod
-    def parse_file(file_path: Path) -> str:
-        """Return the contents of the file as a string."""
-        with open(file_path) as file_:
-            contents: str = file_.read()
-        return contents
+    def parse_file(file_path: Path) -> File:
+        """Parse a file."""
+        with open(file_path) as system_file:
+            contents: str = system_file.read()
+        file_: File = File(file_path, contents)
+        return file_
