88e1b0a91e910a05033e83dae578db8bfee65046
Austin Scola <austinscola@gmail.com>
2020-12-05 16:13:16 -0500

    Fix coverage script

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
+
+set -eu
+
+HERE="$(dirname "$(readlink -f "$BASH_SOURCE")")"
+ARBORISTA="$(realpath "${HERE}/..")"
+
+pushd "${ARBORISTA}" > /dev/null
+trap "popd > /dev/null" EXIT
+
 python3 -m pytest --cov=arborista --cov-report term-missing
