43937521cccdbaf916f85e789eb9de16efecdf31
Austin Scola <austinscola@gmail.com>
2020-12-18 19:43:41 -0500

    Make tree root optional
    Make the root node of a tree optional. Id est, the root of a tree can be
    non-existent.

--- arborista/tree.py
+++ arborista/tree.py
@@ -1,8 +1,10 @@
 """A tree structure."""
+from typing import Optional
+
 from arborista.node import Node
 
 
 class Tree():  # pylint: disable=too-few-public-methods
     """A tree structure."""
-    def __init__(self, root: Node):
-        self.root: Node = root
+    def __init__(self, root: Optional[Node] = None):
+        self.root: Optional[Node] = root
