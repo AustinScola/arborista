f5accb35a50da4076f347da08824a2b634dd5ab5
Austin Scola <austinscola@gmail.com>
2020-12-18 19:54:51 -0500

    Add tree equality operator
    Add the equality operator for trees.

--- arborista/tree.py
+++ arborista/tree.py
@@ -1,5 +1,5 @@
 """A tree structure."""
-from typing import Optional
+from typing import Any, Optional
 
 from arborista.node import Node
 
@@ -8,3 +8,9 @@ class Tree():  # pylint: disable=too-few-public-methods
     """A tree structure."""
     def __init__(self, root: Optional[Node] = None):
         self.root: Optional[Node] = root
+
+    def __eq__(self, other: Any) -> bool:
+        if not isinstance(other, Tree):
+            return False
+
+        return self.root == other.root
