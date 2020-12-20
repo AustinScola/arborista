d439056c5f82f2dbee2f180667864b44dc21f260
Austin Scola <austinscola@gmail.com>
2020-12-20 17:41:13 -0500

    Add testing helper for checking a set of nodes
    Add a testing helper for checking that a set of nodes matches an
    expected set. Right now nodes are mutable and so they cannot be hashable
    which means that they cannot be stored in a set. We should do something
    about this so we don't have to have such an ugly helper method.

---
+++ testing_helpers/assert_nodes_match_expected_nodes.py
@@ -0,0 +1,30 @@
+"""Assertion helper for asserting that a set of nodes matches an expected set of nodes."""
+import pytest
+
+from arborista.node import List, NodeIterator, NodeList
+
+
+def assert_nodes_match_expected_nodes(nodes: NodeIterator, expected_nodes: NodeList) -> None:
+    """Assert that a set of nodes matches an expected set of nodes."""
+    __tracebackhide__ = True  # pylint: disable=unused-variable
+    unexpected_nodes: NodeList = []
+    expected_nodes_not_found = expected_nodes
+
+    for node in nodes:
+        try:
+            expected_nodes_not_found.remove(node)
+        except ValueError:
+            unexpected_nodes.append(node)
+
+    if unexpected_nodes or expected_nodes_not_found:
+
+        failure_messages: List[str] = []
+        if unexpected_nodes:
+            unexpected_nodes_string: str = ' '.join(map(str, unexpected_nodes))
+            failure_messages.append(f'Unexpected node(s): {unexpected_nodes_string}')
+        if expected_nodes_not_found:
+            expected_nodes_string: str = ' '.join(map(str, expected_nodes))
+            failure_messages.append(f'Expected node(s): {expected_nodes_string}')
+
+        message: str = '\n'.join(failure_messages)
+        pytest.fail(message)
