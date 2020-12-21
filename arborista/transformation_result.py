5129b348d94ab1094a177bb4853bbd5b531a1a89
Austin Scola <austinscola@gmail.com>
2020-12-21 13:00:09 -0500

    Add transformation result equality operator
    Add the equality operator for transformation results.
    
    Note that there is some room for optimization when it comes to comparing
    the sets of removed nodes. The conversion of the other removed nodes
    into a set of ids could be lazy.

--- arborista/transformation_result.py
+++ arborista/transformation_result.py
@@ -1,5 +1,5 @@
 """The result of a transformation."""
-from typing import Optional
+from typing import Any, Optional, Set
 
 from arborista.node import Node, NodeIterable, NodeList
 
@@ -11,3 +11,29 @@ class TransformationResult():  # pylint: disable=too-few-public-methods
         self.transformed_node: Optional[Node] = transformed_node
         self.changed: bool = changed
         self.removed_nodes: NodeList = list(removed_nodes)
+
+    def __eq__(self, other: Any) -> bool:
+        if not isinstance(other, TransformationResult):
+            return False
+
+        if self.transformed_node != other.transformed_node:
+            return False
+
+        if self.changed != other.changed:
+            return False
+
+        if len(self.removed_nodes) != len(other.removed_nodes):
+            return False
+
+        unfound_other_removed_node_ids: Set[int] = {
+            id(other_removed_node)
+            for other_removed_node in other.removed_nodes
+        }
+        for removed_node in self.removed_nodes:
+            try:
+                removed_node_id: int = id(removed_node)
+                unfound_other_removed_node_ids.remove(removed_node_id)
+            except KeyError:
+                return False
+
+        return True
--- tests/test_transformation_result.py
+++ tests/test_transformation_result.py
@@ -1,11 +1,11 @@
 """Test arborista.transformation_result."""
-from typing import Optional
+from typing import Any, Optional
 
 import pytest
 
 from arborista.node import Node, NodeIterator, NodeList
 from arborista.transformation_result import TransformationResult
-from testing_helpers.animal_nodes import Dog
+from testing_helpers.animal_nodes import Cat, Dog
 from testing_helpers.assert_nodes_match import assert_nodes_match
 
 
@@ -28,3 +28,37 @@ def test_init(transformed_node: Optional[Node], changed: bool, removed_nodes: No
     assert transformation_result.transformed_node == transformed_node
     assert transformation_result.changed == changed
     assert_nodes_match(transformation_result.removed_nodes, expected_removed_nodes_list)
+
+
+spot = Dog()
+moose = Dog()
+
+aurora = Cat()
+petey = Cat()
+
+
+# yapf: disable # pylint: disable=line-too-long
+@pytest.mark.parametrize('transformation_result, other, expected_equality', [
+    (TransformationResult(None, False, []), 'foo', False),
+    (TransformationResult(Dog(), False, []), TransformationResult(Cat(), False, []), False),
+    (TransformationResult(Dog(), False, []), TransformationResult(Dog(), False, [spot]), False),
+    (TransformationResult(Dog(), False, [spot]), TransformationResult(Dog(), False, [moose]), False),
+    (TransformationResult(Dog(), False, [spot]), TransformationResult(Dog(), False, [petey]), False),
+    (TransformationResult(Dog(), False, [spot, aurora, moose]), TransformationResult(Dog(), False, [spot, petey, moose]), False),
+    (TransformationResult(Dog(), False, [spot, aurora, moose]), TransformationResult(Dog(), False, [spot, aurora, petey]), False),
+    (TransformationResult(Dog(), True, []), TransformationResult(Dog(), False, []), False),
+    (TransformationResult(Dog(), False, []), TransformationResult(Dog(), False, []), True),
+    (TransformationResult(Cat(), True, []), TransformationResult(Cat(), True, []), True),
+    (TransformationResult(None, True, []), TransformationResult(None, True, []), True),
+    (TransformationResult(None, True, [spot]), TransformationResult(None, True, [spot]), True),
+    (TransformationResult(None, True, [aurora, spot]), TransformationResult(None, True, [aurora, spot]), True),
+    (TransformationResult(None, True, [spot, aurora]), TransformationResult(None, True, [aurora, spot]), True),
+    (TransformationResult(None, True, [spot, petey, moose]), TransformationResult(None, True, [petey, spot, moose]), True),
+])
+# yapf: enable # pylint: enable=line-too-long
+def test_eq(transformation_result: TransformationResult, other: Any,
+            expected_equality: bool) -> None:
+    """Test arborista.transformation_result.TransformationResult.__eq__."""
+    equality: bool = transformation_result == other
+
+    assert equality == expected_equality
