36593a15caf8de9c5447fd2787225d3cf27b3ef5
Austin Scola <austinscola@gmail.com>
2020-12-27 16:00:13 -0500

    Add Python pass statement parser

---
+++ tests/parsers/python/test_pass_statement_parser.py
@@ -0,0 +1,24 @@
+"""Test arborista.parsers.python.pass_statement_parser."""
+import libcst
+import pytest
+
+from arborista.nodes.python.pass_statement import PassStatement
+from arborista.parser import Parser
+from arborista.parsers.python.pass_statement_parser import LibcstPassStatement, PassStatementParser
+
+
+def test_inheritance() -> None:
+    """Test arborista.parsers.python.pass_statement_parser.PassStatementParser inheritance."""
+    assert issubclass(PassStatementParser, Parser)
+
+
+# yapf: disable
+@pytest.mark.parametrize('libcst_pass_statement, expected_pass_statement', [
+    (libcst.Pass(), PassStatement()),
+])
+# yapf: enable
+def test_parse_pass_statement(libcst_pass_statement: LibcstPassStatement,
+                              expected_pass_statement: PassStatement) -> None:
+    """Test arborista.parsers.python.pass_statement_parser.PassStatementParser.parse_pass_statement."""  # pylint: disable=line-too-long, useless-suppression
+    pass_statement: PassStatement = PassStatementParser.parse_pass_statement(libcst_pass_statement)
+    assert pass_statement == expected_pass_statement
