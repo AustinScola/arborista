909bd31e78c9e0eb10fb9cd27f86017d59506c6b
Austin Scola <austinscola@gmail.com>
2020-12-28 06:24:10 -0500

    Add deparsing Python pass statements
    Add deparsing pass statements to the small statement deparser.

--- arborista/deparsers/python/small_statement_deparser.py
+++ arborista/deparsers/python/small_statement_deparser.py
@@ -1,7 +1,9 @@
 """Deparser for a Python small statement."""
 from arborista.deparser import Deparser
 from arborista.deparsers.python.flow_statement_deparser import FlowStatementDeparser
+from arborista.deparsers.python.pass_statement_deparser import PassStatementDeparser
 from arborista.nodes.python.flow_statement import FlowStatement
+from arborista.nodes.python.pass_statement import PassStatement
 from arborista.nodes.python.small_statement import SmallStatement
 
 
@@ -10,8 +12,13 @@ class SmallStatementDeparser(Deparser):  # pylint: disable=too-few-public-method
     @staticmethod
     def deparse_small_statement(small_statement: SmallStatement) -> str:
         """Deparse a Python small statement."""
+        string: str
         if isinstance(small_statement, FlowStatement):
             flow_statement: FlowStatement = small_statement
-            string: str = FlowStatementDeparser.deparse_flow_statement(flow_statement)
+            string = FlowStatementDeparser.deparse_flow_statement(flow_statement)
+            return string
+        if isinstance(small_statement, PassStatement):
+            pass_statement: PassStatement = small_statement
+            string = PassStatementDeparser.deparse_pass_statement(pass_statement)
             return string
         raise NotImplementedError
