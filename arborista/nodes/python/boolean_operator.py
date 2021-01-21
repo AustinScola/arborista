43e6f0c7e4f87a4c6e4911b1c0c2c6a777e153ef
Austin Scola <austinscola@gmail.com>
2021-01-21 17:32:42 -0500

    Add Python boolean operator node
    Add a node for Python boolean operators.

---
+++ arborista/nodes/python/boolean_operator.py
@@ -0,0 +1,8 @@
+"""A Python boolean operator."""
+from abc import ABC
+
+from arborista.nodes.python.python_node import PythonNode
+
+
+class BooleanOperator(PythonNode, ABC):
+    """A Python boolean operator."""
