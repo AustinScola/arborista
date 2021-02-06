7d83b546b8e0376398639e805a4a3c60a32bb419
Austin Scola <austinscola@gmail.com>
2021-02-05 20:13:38 -0500

    Add Python expressions
    Add an iterable of Python expressions.

--- arborista/nodes/python/expression.py
+++ arborista/nodes/python/expression.py
@@ -1,6 +1,11 @@
 """A Python expression."""
+from typing import Iterable
+
 from arborista.nodes.python.python_node import PythonNode
 
 
 class Expression(PythonNode):
     """A Python expression."""
+
+
+Expressions = Iterable[Expression]
