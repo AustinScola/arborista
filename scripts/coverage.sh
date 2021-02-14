c66b8f1a3f2b1a13fac31e404dde70dc3941a4b9
Austin Scola <austinscola@gmail.com>
2021-02-14 06:41:46 -0500

    Change scripts to use venvs
    Change scripts to use virtual environments.

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -8,4 +8,7 @@ ARBORISTA="$(realpath "${HERE}/..")"
 pushd "${ARBORISTA}" > /dev/null
 trap "popd > /dev/null" EXIT
 
+source "${ARBORISTA}/scripts/library/venv.sh"
+use_venv "test" frozen_test_requirements.txt
+
 python3 -m pytest --cov=arborista --cov-report term-missing
