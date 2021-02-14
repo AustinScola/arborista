797d600757f4b14bd6cf99cd30ca92b7760c6489
Austin Scola <austinscola@gmail.com>
2021-02-14 08:48:41 -0500

    Change formatting file finding
    Change the way that files are found when formatting to rely on the tools
    instead of the `find` command.

--- scripts/format.sh
+++ scripts/format.sh
@@ -8,6 +8,8 @@ ARBORISTA="$(realpath "${HERE}/..")"
 pushd "${ARBORISTA}" > /dev/null
 trap "popd > /dev/null" EXIT
 
-find . -name "*.py" | xargs python3 -m yapf -i
+source "${ARBORISTA}/scripts/library/venv.sh"
+use_venv "developer" frozen_developer_requirements.txt
 
-find . -name "*.py" | xargs python3 -m isort -i
+python3 -m yapf -i -r .
+python3 -m isort .
