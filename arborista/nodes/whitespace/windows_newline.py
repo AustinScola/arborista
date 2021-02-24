7dd841c248948c07c8238cfd1d5f0dfb2baa1379
Austin Scola <austinscola@gmail.com>
2021-02-23 21:01:02 -0500

    Use Seligimus equal type decorator

--- arborista/nodes/whitespace/windows_newline.py
+++ arborista/nodes/whitespace/windows_newline.py
@@ -1,13 +1,16 @@
 """Windows newline characters."""
 from typing import Any
 
+from seligimus.python.decorators.operators.equality.equal_type import equal_type
+
 from arborista.nodes.whitespace.newline import Newline
 
 
 class WindowsNewline(Newline):
     """Windows newline characters."""
+    @equal_type
     def __eq__(self, other: Any) -> bool:
-        return isinstance(other, WindowsNewline)
+        return True
 
     def __str__(self) -> str:
         return '\r\n'
