6730baba124116c24fc0573f341b215770534b85
Austin Scola <austinscola@gmail.com>
2021-02-25 07:18:10 -0500

    Add NEBNF node
    Add a Named Extended Backus-Naur Form node.

---
+++ arborista/nodes/nebnf/nebnf_node.py
@@ -0,0 +1,8 @@
+"""A Named Extended Backus-Naur Form node."""
+from abc import ABC
+
+from arborista.node import Node
+
+
+class NEBNFNode(Node, ABC):
+    """A Named Extended Backus-Naur Form node."""
