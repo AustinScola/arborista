cc91bb478550f054d2b534e4546e0d73f276c3a9
Austin Scola <austinscola@gmail.com>
2021-03-27 07:11:41 -0400

    Add righthandside NEBNF node
    Add a righthandside NEBNF node. This is not complete yet, it only
    includes the options which are defined so far. The other ones will be
    added in succession.

---
+++ arborista/nodes/nebnf/righthand_side.py
@@ -0,0 +1,24 @@
+"""The righthand side of a production rule."""
+from typing import Any, Optional, Union
+
+from seligimus.python.decorators.operators.equality.equal_type import equal_type
+
+from arborista.node import Node
+from arborista.nodes.nebnf.identifier import Identifier
+from arborista.nodes.nebnf.nebnf_node import NEBNFNode
+from arborista.nodes.nebnf.terminal import Terminal
+
+Expression = Union[Terminal, Identifier]
+
+
+class RighthandSide(NEBNFNode):
+    """The righthand side of a production rule."""
+    def __init__(self, expression: Expression, parent: Optional[Node] = None):
+        super().__init__(parent)
+
+        self.expression: Expression = expression
+
+    @equal_type
+    def __eq__(self, other: Any) -> bool:
+        equality: bool = self.expression == other.expression
+        return equality
