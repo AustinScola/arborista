8215cfa91c2a8e481d33127ebaacee1787b0ed04
Austin Scola <austinscola@gmail.com>
2021-04-02 17:30:09 -0400

    Add Python comparison operator node

---
+++ arborista/nodes/python/comparison_operator.py
@@ -0,0 +1,8 @@
+"""A Python comparison operator."""
+from abc import ABC
+
+from arborista.nodes.python.python_node import PythonNode
+
+
+class ComparisonOperator(PythonNode, ABC):
+    """A Python comparison operator."""
