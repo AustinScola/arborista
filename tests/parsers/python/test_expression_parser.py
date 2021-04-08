ebcb7d8bc2d2967f49d23dadce1b943ed453bc43
Austin Scola <austinscola@gmail.com>
2021-04-08 18:10:19 -0400

    Add Python expression parser

---
+++ tests/parsers/python/test_expression_parser.py
@@ -0,0 +1,31 @@
+"""Test arborista.parsers.python.expression_parser."""
+import libcst
+import pytest
+
+from arborista.nodes.python.expression import Expression
+from arborista.nodes.python.integer import Integer
+from arborista.parser import Parser
+from arborista.parsers.python.expression_parser import ExpressionParser, LibcstExpression
+
+
+def test_libcst_expression() -> None:
+    """Test arborista.parsers.python.expression_parser.LibcstExpression."""
+    assert LibcstExpression == libcst.BaseExpression
+
+
+def test_inheritance() -> None:
+    """Test arborista.parsers.python.expression_parser.ExpressionParser inheritance."""
+    assert issubclass(ExpressionParser, Parser)
+
+
+# yapf: disable
+@pytest.mark.parametrize('libcst_expression, expected_expression', [
+    (libcst.Integer('1'), Integer(1)),
+])
+# yapf: enable
+def test_parse_expression(libcst_expression: LibcstExpression,
+                          expected_expression: Expression) -> None:
+    """Test arborista.parsers.python.expression_parser.ExpressionParser.parse_expression."""
+    expression: Expression = ExpressionParser.parse_expression(libcst_expression)
+
+    assert expression == expected_expression
