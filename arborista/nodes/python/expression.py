612abbab59dc464b3c3c2547a17ec86b9b02a84a
Austin Scola <austinscola@gmail.com>
2021-04-18 16:39:00 -0400

    Change a Python expression to be an argument

--- arborista/nodes/python/expression.py
+++ arborista/nodes/python/expression.py
@@ -1,10 +1,11 @@
 """A Python expression."""
 from typing import Iterable
 
+from arborista.nodes.python.argument import Argument
 from arborista.nodes.python.python_node import PythonNode
 
 
-class Expression(PythonNode):
+class Expression(Argument, PythonNode):
     """A Python expression."""
 
 
--- arborista/nodes/python/integer.py
+++ arborista/nodes/python/integer.py
@@ -5,7 +5,7 @@ from arborista.node import Node
 from arborista.nodes.python.number import Number
 
 
-class Integer(Number):
+class Integer(Number):  # pylint: disable=too-many-ancestors
     """A Python integer."""
     def __init__(self, value: int, parent: Optional[Node] = None):
         super().__init__(parent)
