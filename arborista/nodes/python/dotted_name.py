8a1f971a9225cb89a158e6f18537141e365da447
Austin Scola <austinscola@gmail.com>
2021-04-21 06:56:21 -0400

    Fix dotted name node docstring

--- arborista/nodes/python/dotted_name.py
+++ arborista/nodes/python/dotted_name.py
@@ -7,7 +7,7 @@ from arborista.nodes.python.name import Name, Names
 
 
 class DottedName(ImportStatement):
-    """A Python import dotted name."""
+    """A Python dotted name."""
     def __init__(self, first_name: Name, rest_of_names: Names, parent: Optional[Node] = None):
         super().__init__(parent)
 
