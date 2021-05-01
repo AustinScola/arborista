7408d24fdd938a4a31ff177fa936dd73f9543ef3
Austin Scola <austinscola@gmail.com>
2021-05-01 06:30:33 -0400

    Add Python if deparser

---
+++ arborista/deparsers/python/if_deparser.py
@@ -0,0 +1,30 @@
+"""Deparser for a Python if statement."""
+from arborista.deparser import Deparser
+from arborista.deparsers.python.suite_deparser import SuiteDeparser
+from arborista.nodes.python.block import Block
+from arborista.nodes.python.expression import Expression
+from arborista.nodes.python.if_ import If
+from arborista.nodes.python.suite import Suite
+
+
+class IfDeparser(Deparser):  # pylint: disable=too-few-public-methods
+    """Deparser for a Python if statement."""
+    @staticmethod
+    def deparse_if(if_: If) -> str:
+        """Deparse a Python if statement."""
+        string: str
+
+        from arborista.deparsers.python.expression_deparser import \
+            ExpressionDeparser  # pylint: disable=import-outside-toplevel
+        condition: Expression = if_.condition
+        condition_string: str = ExpressionDeparser.deparse_expression(condition)
+
+        body: Suite = if_.body
+        if isinstance(body, Block):
+            body_string = '\n' + SuiteDeparser.deparse_suite(body, '')
+        else:
+            body_string = SuiteDeparser.deparse_suite(body, '')
+
+        string = f'if {condition_string}:{body_string}'
+
+        return string
