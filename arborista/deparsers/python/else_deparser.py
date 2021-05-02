cbedd8cf3de79cb606b1e786cee0cd92f746bdb7
Austin Scola <austinscola@gmail.com>
2021-05-02 13:29:31 -0400

    Add else indentation

--- arborista/deparsers/python/else_deparser.py
+++ arborista/deparsers/python/else_deparser.py
@@ -9,16 +9,16 @@ from arborista.nodes.python.suite import Suite
 class ElseDeparser(Deparser):  # pylint: disable=too-few-public-methods
     """Deparser for a Python else statement."""
     @staticmethod
-    def deparse_else(else_: Else) -> str:
+    def deparse_else(else_: Else, indent: str) -> str:
         """Deparse a Python else statement."""
         string: str
 
         body: Suite = else_.body
         if isinstance(body, Block):
-            body_string = '\n' + SuiteDeparser.deparse_suite(body, '')
+            body_string = '\n' + SuiteDeparser.deparse_suite(body, indent)
         else:
             body_string = SuiteDeparser.deparse_suite(body, '')
 
-        string = f'else:{body_string}'
+        string = f'{indent}else:{body_string}'
 
         return string
