9ef9bfc4f43cbc77a00941325fd092fcbd74818b
Austin Scola <austinscola@gmail.com>
2021-05-02 06:31:42 -0400

    Add Python dotted name atom deparsing
    Add deparsing of dotted names as an atom.

--- arborista/deparsers/python/atom_deparser.py
+++ arborista/deparsers/python/atom_deparser.py
@@ -1,8 +1,10 @@
 """A deparser for a Python atom."""
 from arborista.deparser import Deparser
+from arborista.deparsers.python.dotted_name_deparser import DottedNameDeparser
 from arborista.deparsers.python.name_deparser import NameDeparser
 from arborista.deparsers.python.string_deparser import StringDeparser
 from arborista.nodes.python.atom import Atom
+from arborista.nodes.python.dotted_name import DottedName
 from arborista.nodes.python.name import Name
 from arborista.nodes.python.string import String
 
@@ -20,6 +22,9 @@ class AtomDeparser(Deparser):  # pylint: disable=too-few-public-methods
         elif isinstance(atom, Name):
             name: Name = atom
             string = NameDeparser.deparse_name(name)
+        elif isinstance(atom, DottedName):
+            dotted_name: DottedName = atom
+            string = DottedNameDeparser.deparse_dotted_name(dotted_name)
         else:
             raise NotImplementedError  # pragma: no cover
 
