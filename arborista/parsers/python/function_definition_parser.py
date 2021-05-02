afcd440773777a0ceb2c25f9e99f3b2f6f362365
Austin Scola <austinscola@gmail.com>
2021-05-02 12:38:49 -0400

    Add function definition returns parsing
    Add parsing of the funciton definition return annotations.

--- arborista/parsers/python/function_definition_parser.py
+++ arborista/parsers/python/function_definition_parser.py
@@ -1,11 +1,15 @@
 """Parser for a Python function definition."""
+from typing import Optional
+
 import libcst
 
+from arborista.nodes.python.expression import Expression
 from arborista.nodes.python.function_definition import FunctionDefinition
 from arborista.nodes.python.name import Name
 from arborista.nodes.python.parameter import ParameterList
 from arborista.nodes.python.suite import Suite
 from arborista.parser import Parser
+from arborista.parsers.python.expression_parser import ExpressionParser
 from arborista.parsers.python.name_parser import LibcstName, NameParser
 from arborista.parsers.python.parameter_parser import LibcstParameters, ParameterParser
 
@@ -29,7 +33,15 @@ class FunctionDefinitionParser(Parser):  # pylint: disable=too-few-public-method
         libcst_body: LibcstSuite = libcst_function_definition.body
         body: Suite = SuiteParser.parse_suite(libcst_body)
 
-        function_definition: FunctionDefinition = FunctionDefinition(name, parameters, body)
+        returns: Optional[Expression]
+        if libcst_function_definition.returns is None:
+            returns = None
+        else:
+            libcst_returns = libcst_function_definition.returns.annotation
+            returns = ExpressionParser.parse_expression(libcst_returns)
+
+        function_definition: FunctionDefinition = FunctionDefinition(name, parameters, body,
+                                                                     returns)
         function_definition.set_parent_in_children()
 
         return function_definition
