e63b46aa5fb776d750b296b0576aeda680b7493d
Austin Scola <austinscola@gmail.com>
2021-05-05 08:58:13 -0400

    Add return statement value

--- arborista/nodes/python/return_statement.py
+++ arborista/nodes/python/return_statement.py
@@ -1,7 +1,14 @@
 """A Python return statement."""
+from typing import Optional
 
+from arborista.node import Node
+from arborista.nodes.python.expression import Expression
 from arborista.nodes.python.flow_statement import FlowStatement
 
 
 class ReturnStatement(FlowStatement):
     """A Python return statement."""
+    def __init__(self, value: Optional[Expression] = None, parent: Optional[Node] = None):
+        super().__init__(parent)
+
+        self.value: Optional[Expression] = value
--- tests/nodes/python/test_return_statement.py
+++ tests/nodes/python/test_return_statement.py
@@ -1,8 +1,36 @@
 """Test arborista.nodes.python.return_statement."""
+from typing import Any, Dict, List, Optional
+from unittest.mock import MagicMock
+
+import pytest
+
+from arborista.node import Node
+from arborista.nodes.python.expression import Expression
 from arborista.nodes.python.flow_statement import FlowStatement
+from arborista.nodes.python.name import Name
 from arborista.nodes.python.return_statement import ReturnStatement
 
 
 def test_inheritance() -> None:
     """Test arborista.nodes.python.return_statement.ReturnStatement inheritance."""
     assert issubclass(ReturnStatement, FlowStatement)
+
+
+_parent = MagicMock()
+
+
+# yapf: disable
+@pytest.mark.parametrize('arguments, keyword_arguments, expected_value, expected_parent', [
+    ([], {}, None, None),
+    ([Name('foo')], {}, Name('foo'), None),
+    ([], {'parent': None}, None, None),
+    ([], {'parent': _parent}, None, _parent),
+])
+# yapf: enable
+def test_init(arguments: List[Any], keyword_arguments: Dict[str, Any],
+              expected_value: Optional[Expression], expected_parent: Optional[Node]) -> None:
+    """Test arborista.nodes.python.return_statement.ReturnStatement.__init__."""
+    return_statement: ReturnStatement = ReturnStatement(*arguments, **keyword_arguments)
+
+    assert return_statement.value == expected_value
+    assert return_statement.parent is expected_parent
