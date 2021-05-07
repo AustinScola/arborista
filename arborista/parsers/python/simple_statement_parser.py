1c489ad245c6ec60c714af4d9de8c35941d7c050
Austin Scola <austinscola@gmail.com>
2021-05-07 10:56:07 -0400

    Add simple statement trailing whitespace parsing

--- arborista/parsers/python/simple_statement_parser.py
+++ arborista/parsers/python/simple_statement_parser.py
@@ -5,9 +5,12 @@ import libcst
 
 from arborista.nodes.python.simple_statement import SimpleStatement
 from arborista.nodes.python.small_statement import SmallStatementList
+from arborista.nodes.python.trailing_whitespace import TrailingWhitespace
 from arborista.parser import Parser
 from arborista.parsers.python.small_statement_parser import (LibcstSmallStatements,
                                                              SmallStatementParser)
+from arborista.parsers.python.trailing_whitespace_parser import (LibcstTrailingWhitespace,
+                                                                 TrailingWhitespaceParser)
 
 LibcstSimpleStatement = Union[libcst.SimpleStatementLine, libcst.SimpleStatementSuite]
 
@@ -22,7 +25,12 @@ class SimpleStatementParser(Parser):  # pylint: disable=too-few-public-methods
         small_statements: SmallStatementList = SmallStatementParser.parse_small_statements(
             libcst_small_statements)
 
-        simple_statement: SimpleStatement = SimpleStatement(small_statements)
+        libcst_trailing_whitespace: LibcstTrailingWhitespace = \
+            libcst_simple_statement.trailing_whitespace
+        trailing_whitespace: TrailingWhitespace = \
+            TrailingWhitespaceParser.parse_trailing_whitespace(libcst_trailing_whitespace)
+
+        simple_statement: SimpleStatement = SimpleStatement(small_statements, trailing_whitespace)
         simple_statement.set_parent_in_children()
 
         return simple_statement
--- tests/parsers/python/test_simple_statement_parser.py
+++ tests/parsers/python/test_simple_statement_parser.py
@@ -4,6 +4,8 @@ import pytest
 
 from arborista.nodes.python.return_statement import ReturnStatement
 from arborista.nodes.python.simple_statement import SimpleStatement
+from arborista.nodes.python.simple_whitespace import SimpleWhitespace
+from arborista.nodes.python.trailing_whitespace import TrailingWhitespace
 from arborista.parser import Parser
 from arborista.parsers.python.simple_statement_parser import (LibcstSimpleStatement,
                                                               SimpleStatementParser)
@@ -15,11 +17,12 @@ def test_inheritance() -> None:
     assert issubclass(SimpleStatementParser, Parser)
 
 
-# yapf: disable
+# yapf: disable # pylint: disable=line-too-long
 @pytest.mark.parametrize('libcst_simple_statement, expected_simple_statement', [
     (libcst.SimpleStatementLine([libcst.Return()]), SimpleStatement([ReturnStatement()])),
+    (libcst.SimpleStatementLine([libcst.Return()], trailing_whitespace=libcst.TrailingWhitespace(libcst.SimpleWhitespace(' '))), SimpleStatement([ReturnStatement()], TrailingWhitespace(SimpleWhitespace(' ')))),
 ])
-# yapf: enable
+# yapf: enable # pylint: enable=line-too-long
 def test_parse_simple_statement(libcst_simple_statement: LibcstSimpleStatement,
                                 expected_simple_statement: SimpleStatement) -> None:
     """Test arborista.parsers.python.simple_statement_parser.SimpleStatementParser.parse_simple_statement."""  # pylint: disable=line-too-long, useless-suppression
